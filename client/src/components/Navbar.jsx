baf5f1b2e7666bdf2cca4781bb1947800123f486
smitzo <joshismit2812@gmail.com>
2025-06-02 11:50:34 +0530

    [ADD] add basic components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <>
+    <div className="flex w-[1000px] bg-[red]">Klipit</div>
+    <span className="">Online clipboard. Copy data between devices without installing an app. Klipit Pro is now live.</span>
+    
+    </>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/TextEditor.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+
+const TextEditor = () => {
+  return (
+    <div>
+        <p>Enter your text</p>
+        <textarea class="border-4 border-indigo-500"></textarea>
+    </div>
+  )
+}
+
+export default TextEditor
